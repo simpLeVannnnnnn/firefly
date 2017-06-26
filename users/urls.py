36d4516d93ba452678a7cc4c39d5c5f65afd0c5d
simpLeVannnnnnn <286417986@qq.com>
2017-06-26 16:18:42 +0800

    base

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.conf.urls import patterns, url
+from users import views
+
+
+urlpatterns = patterns('',
+    url(r'^$', views.login, name='login'),
+    url(r'^login/$',views.login,name = 'login'),
+    url(r'^regist/$',views.regist,name = 'regist'),
+    url(r'^index/$',views.index,name = 'index'),
+    url(r'^logout/$',views.logout,name = 'logout'),
+)
